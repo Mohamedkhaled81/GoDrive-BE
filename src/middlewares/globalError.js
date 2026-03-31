277f5af08af3cffae4aef1a7267f8d46884c3afb
Mohamed Khaled <mohamed.khaled.200540@gmail.com>
2026-03-31 20:56:37 +0200

    HotFix/ making the global Error Object returns a JSON

--- src/middlewares/globalError.js
+++ src/middlewares/globalError.js
@@ -1,4 +1,10 @@
 export default function (error, req, res, next) {
     error.statusCode = error.statusCode || 500;
-    res.render('err', {message: error.message, status: error.statusCode});
+    res.status(error.statusCode).json(
+        {
+            error: {
+                message: error.message,
+                status: error.statusCode
+            }
+        });
 }
