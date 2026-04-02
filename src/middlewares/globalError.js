5bf024a0be139dad57c4f84832b9e344ebb2f5f7
Mohamed Khaled <mohamed.khaled.200540@gmail.com>
2026-04-02 22:43:41 +0200

    DevErr ProdErr handeling

--- src/middlewares/globalError.js
+++ src/middlewares/globalError.js
@@ -1,10 +1,44 @@
-export default function (error, req, res, next) {
-    error.statusCode = error.statusCode || 500;
+const devErr = (error, res) => {
     res.status(error.statusCode).json(
         {
             error: {
                 message: error.message,
-                status: error.statusCode
+                status: error.statusCode,
+                stackTrace: error.stack,
+                error: error
             }
-        });
+        }
+    );
+}
+
+const prodErr = (error, res) => {
+    if (error.isOperational) {
+        res.status(error.statusCode).json(
+            {
+                error: {
+                    message: error.message,
+                    status: error.statusCode
+                }
+            }
+        );
+    } else {
+        res.status(500).json(
+            {
+                error: {
+                    message: 'Something Went Wrong!',
+                    status: 500
+                }
+            }
+        );
+    }
+}
+
+
+export default function (error, req, res, next) {
+    error.statusCode = error.statusCode || 500;
+    if (process.env.Node_ENV === 'development') {
+        devErr(error, res);
+    } else {
+        prodErr(error, res);
+    }
 }
